fee409e155a86ce7735622357ff0ca806e7a9c23
ABY ABRAHAM KALATHIVILA <abyabrkal@gmail.com>
2018-03-15 10:01:09 +0400

    Formatted Display and Cleaning up
    Output data is formatted display multiple info, if available, Comments
    added.

--- dictionary/dict.py
+++ dictionary/dict.py
@@ -17,28 +17,40 @@
 import json
 from difflib import get_close_matches
 
-
+#load dictionary data from json
 data = json.load(open("dict.json"))
 
 
 def translate(w):
+	# words to its lowercase to making search case insensitive
 	w = w.lower()
+
+	# search in dict for the word
 	if w in data:
 		return data[w]
+	# if not, check for any close matches - Close Match Algorithm
 	elif len(get_close_matches(w, data.keys())) > 0:
+		# get the first(best) choice from closest matches ratio
 		best_choice = get_close_matches(w, data.keys())[0]
 		yesno = input("Did you mean %s instead? Enter Y if yes , or N if no:" % best_choice).lower()
 		if yesno == 'y':
 			return data[best_choice]
 		elif yesno == 'n':
-			return "The word doesn't exist. PLease check!."
+			return "The word doesn't exist. Please check!."
+		# no close matches
 		else:
-			return "We didn't understand your entry"
+			return "We didn't understand your entry."
 	else:
 		return "The word doesn't exist. Please check!."
 
 
 
 word = input("Enter your word: ")
+output = translate(word)
 
-print(translate(word))
+# lists our multiple info separately, if available.
+if type(output) == list:
+	for info in output:
+		print(info)
+else:
+	print(output)
