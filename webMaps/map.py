8dbd5a4dfc3fbe7f418e0f156d717958959b5a41
ABY ABRAHAM KALATHIVILA <abyabrkal@gmail.com>
2018-03-18 10:42:16 +0400

    GeoMap completed
    Geomap with volcano and population data layered in world map (mapbox
    bright map) using Folium package.

--- webMaps/map.py
+++ webMaps/map.py
@@ -20,12 +20,12 @@ def color_producer(elevation):
 #map = folium.Map(location=[38.56, -99.09], zoom_start=6)
 map = folium.Map(location=[38.56, -99.09], zoom_start=6, tiles="Mapbox Bright")
 
-fg = folium.FeatureGroup(name="My Map")
-
-
+# A Child layer created for placing Volcano makers 
+#   and added as a separate feature group
+fgv = folium.FeatureGroup(name="Volcanoes")
 
 for lt, ln, el in zip(lat, lon, elev):
-	fg.add_child(folium.CircleMarker(location=[lt, ln],
+	fgv.add_child(folium.CircleMarker(location=[lt, ln],
 					radius=6, 
 					#Or, popup=folium.Popup(str(el), parse_html=true))
 					popup=str(el)+"m", 
@@ -34,7 +34,18 @@ for lt, ln, el in zip(lat, lon, elev):
 					fill=True,
 					fill_opacity=0.7))
 
+# Second Child layer created for placing Population marked from multipolygons
+#   and added as a separate feature group
+fgp = folium.FeatureGroup(name="Population")
 
+fgp.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(),
+	style_function=lambda x: {'fillColor':'yellow' if x['properties']['POP2005'] < 10000000 else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
 
-map.add_child(fg)
-map.save("Map2.html")
+#add both childs to base map
+map.add_child(fgv)
+map.add_child(fgp)
+
+# add a layer control for selecting/deselecting the controls
+map.add_child(folium.LayerControl())
+
+map.save("GeoMap-VolcanoPopulation.html")
